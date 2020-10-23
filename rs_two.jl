d14203f7547d9afaaf96e843ab45640ed0ca2909
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-10-22 22:18:26 -0400

    Added 2 state variable case

--- rs_two.jl
+++ rs_two.jl
@@ -23,6 +23,19 @@ function calcdvthetaclassic!(du, u, p, t)
 end
 
 
+function calcdv2theta!(du, u, p, t)
+    dc, eta, sigman, a, b1, b2, mu, Vp, L, rho, statelaw = p
+    theta1 = u[1]
+    theta2 = u[2]
+    v = u[3]
+    dtau1_dt = mu * (Vp - v) / L
+    du[1] = statelaw(v, theta1, dc)
+    du[2] = statelaw(v, theta2, dc)
+    du[3] = 1 / (eta / sigman + a / v) * (dtau1_dt / sigman - b1 * du[1] / theta1 - b2 * du[2] / theta2)
+    return nothing
+end
+
+
 function calcdvthetatwo!(du, u, p, t)
     dc, eta, sigman, a, b, mu, Vp, L, rho, statelaw = p
 
@@ -33,9 +46,9 @@ function calcdvthetatwo!(du, u, p, t)
     theta3 = u[5]
     v3 = u[6]
 
-    dtau1_dt = mu * (Vp - (v1 - 0.01*v2 - 0.04*v3)) / L
-    dtau2_dt = mu * (Vp - (v2 - 0.05*v1 - 0.06*v3)) / L
-    dtau3_dt = mu * (Vp - (v3 - 0.03*v1 - 0.01*v2)) / L
+    dtau1_dt = mu * (Vp - (v1 + 0.01*v2 + 0.04*v3)) / L
+    dtau2_dt = mu * (Vp - (v2 + 0.05*v1 + 0.95*v3)) / L
+    dtau3_dt = mu * (Vp - (v3 + 0.03*v1 - 0.95*v2)) / L
     
     du[1] = statelaw(v1, theta1, dc)
     du[2] = 1 / (eta / sigman + a / v1) * (dtau1_dt / sigman - b * du[1] / theta1)
@@ -64,17 +77,12 @@ function rs_two()
     vp = 1e-9
     sigman = 30e6
     dc = 0.1
-    abstol = 1e-4
-    reltol = 1e-4
-
-    # Parameters need for d(a, b simgan)/dt
-    fstar = 0.6 # ???
-    vstar = vp # ???
-    thetastar = 1e9 # ???
+    abstol = 1e-8
+    reltol = 1e-8
 
     # Time integrate - classic
     icsclassic = [1e8; vp / 1000]
-    pclassic = (dc, eta, sigman, a, b, mu, vp, L, rho, aginglaw)
+    pclassic = (dc, eta, sigman, a, b, mu, vp, L, rho, sliplaw)
     probclassic = ODEProblem(calcdvthetaclassic!, icsclassic, tspan, pclassic)
     solclassic = solve(probclassic, RK4(), abstol = abstol, reltol = reltol)
 
@@ -89,10 +97,30 @@ function rs_two()
     xlabel("time (years)")
     ylabel("v (m/s)")
 
+
+    # Time integrate - two state parameters
+    icsclassic = [1e8; 2e8; vp / 1000]
+    pclassic = (dc, eta, sigman, a, b, 0.99*b, mu, vp, L, rho, sliplaw)
+    probclassic = ODEProblem(calcdv2theta!, icsclassic, tspan, pclassic)
+    solclassic = solve(probclassic, RK4(), abstol = abstol, reltol = reltol)
+
+    # Plot velocity time series
+    t1 = [x / siay for x in solclassic.t]
+    theta1 = [x[1] for x in solclassic.u]
+    v1 = [x[3] for x in solclassic.u]
+
+    figure()
+    plot(t1, v1, "-b", linewidth = 0.5)
+    yscale("log")
+    xlabel("time (years)")
+    ylabel("v (m/s)")
+    return
+    
+    
     # Time integrate - multi
     icstwo = [1e8; vp / 1000; 1e8; vp / 1000; 1e8 ; vp / 1000]
 
-    ptwo = (dc, eta, sigman, a, b, mu, vp, L, rho, aginglaw)
+    ptwo = (dc, eta, sigman, a, b, mu, vp, L, rho, sliplaw)
     probtwo = ODEProblem(calcdvthetatwo!, icstwo, tspan, ptwo)
     soltwo = solve(probtwo, RK4(), abstol = abstol, reltol = reltol)
 
@@ -106,13 +134,46 @@ function rs_two()
 
     figure()
     plot(t1, v1, "-b", linewidth = 0.5)
-    plot(t1, v2, "--r", linewidth = 0.5)
-    plot(t1, v3, ":g", linewidth = 0.5)
-    
     yscale("log")
     xlabel("time (years)")
     ylabel("v (m/s)")
+
+    figure()
+    plot(t1, v2, "--r", linewidth = 0.5)
+    yscale("log")
+    xlabel("time (years)")
+    ylabel("v (m/s)")
+
+    figure()
+    plot(v3, "-g", linewidth = 0.5)
+    yscale("log")
+    xlabel("time (years)")
+    ylabel("v (m/s)")
+
+    figure()
+    subplot(2, 2, 1)
+    plot(log10.(v1), log10.(v1), "-k", linewidth = 0.1)
+    subplot(2, 2, 2)
+    plot(log10.(v1), log10.(v3), "-k", linewidth = 0.1)
+    subplot(2, 2, 3)
+    plot(log10.(v2), log10.(v3), "-k", linewidth = 0.1)
     
+    d1 = zeros(length(v1))
+    d2 = zeros(length(v1))
+    d3 = zeros(length(v1))
+    for i in 2:length(v1)
+        d1[i] = d1[i-1] + (v1[i-1]) / (t1[i] - t1[i-1])
+        d2[i] = d2[i-1] + (v2[i-1]) / (t1[i] - t1[i-1])
+        d3[i] = d3[i-1] + (v3[i-1]) / (t1[i] - t1[i-1])
+    end
+    figure()
+    plot(t1, d1, "-b", linewidth = 0.5)
+    plot(t1, d2, "-r", linewidth = 0.5)
+    plot(t1, d3, "-g", linewidth = 0.5)
+
+    xlabel("time (years)")
+    ylabel("d (m)")
+
     
     return nothing
 end
